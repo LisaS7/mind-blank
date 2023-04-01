53d56430aa3e85c90fd1bac32e15eec20692f00c
Lisa <mclisa00@gmail.com>
2023-04-01 06:26:11 +0100

    Refactored start animation to separate component

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,11 @@
 import { useState } from "react";
-import { motion } from "framer-motion";
 import { useSelector, useDispatch } from "react-redux";
 import { setStatus } from "./state/quizSlice";
 import "./App.css";
 import Intro from "./components/Intro/Intro";
 import GameContainer from "./containers/GameContainer";
 import MusicPlayer from "./components/Music/MusicPlayer";
-import fullCurtain from "./components/Intro/curtain-full.png";
-import startShow from "./components/Intro/start-show-button.png";
-
-const startButtonVariants = {
-  initial: { scale: 0 },
-  animate: {
-    scale: 1,
-    y: [0, 25, 0],
-    transition: {
-      delay: 1.25,
-      scale: { duration: 1 },
-      y: { repeat: Infinity, duration: 1.5 },
-    },
-  },
-};
-
-const curtainVariants = {
-  initial: { y: "-100vh" },
-  animate: { y: 0, transition: { duration: 2 } },
-};
+import Start from "./components/Intro/start";
 
 function App() {
   const { status } = useSelector((state) => state.quiz);
@@ -45,28 +25,8 @@ function App() {
 
   switch (status) {
     case "start":
-      return (
-        <div className="app-container">
-          <motion.img
-            className="full-curtain"
-            initial="initial"
-            animate="animate"
-            variants={curtainVariants}
-            src={fullCurtain}
-          />
-          <motion.img
-            initial="initial"
-            animate="animate"
-            variants={startButtonVariants}
-            className="start-app-button"
-            src={startShow}
-            alt="start show button"
-            onClick={() => {
-              dispatch(setStatus("intro"));
-            }}
-          />
-        </div>
-      );
+      return <Start />;
+
     case "intro":
       return (
         <div className="app-container">
